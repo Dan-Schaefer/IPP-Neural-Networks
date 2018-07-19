247e51a7887f71c180169ed850eaaac6d8a871bf
Ephecina <danielschaefer01@gmail.com>
2018-07-19 19:57:51 +0200

    Repository Update
    2018-07-19
    
    Fixed Neural Network from 3 hidden layers to 2.
    
    Added Histograms to TensorBoard callback.
    
    Playing around with different initial weights initializations.

---
+++ 2018-07-19_Run0052.py
@@ -0,0 +1,148 @@
+#! /usr/bin/env python
+
+'''
+Trains 7D QuaLiKiz-NN with a single output (efiTG)
+'''
+
+from __future__ import print_function
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization
+from keras.optimizers import RMSprop, adam, Adam
+from keras.initializers import TruncatedNormal
+from keras import regularizers
+from keras import backend as K
+import pandas
+import numpy
+import sys
+import os
+
+# Define new Metric: rmse = Root Mean Square Error
+def rmse(y_true, y_pred):
+    return K.sqrt(K.mean(K.square( y_true-y_pred )))
+
+# Gets the current file name. Useful for procedurally generating output/log files.
+file_name =  os.path.basename(sys.argv[0][:-3])
+
+# Define neural network parameters
+batch_size = 10
+#num_classes = 1
+epochs = 100
+
+# Load Data (which is in HDF5 or .h5 format)
+store = pandas.HDFStore("unstable_training_gen3_7D_nions0_flat_filter8.h5")
+target_df = store['/output/efeETG_GB'].to_frame()  # This one is relatively easy to train
+input_df = store['input']
+
+# Puts inputs and outputs in the same pandas dataframe.
+# Also only keeps overlapping entries.
+joined_dataFrame = target_df.join(input_df)
+
+# Normalize data by standard deviation and mean-centering the data
+joined_dataFrame['efeETG_GB'] = (joined_dataFrame['efeETG_GB'] - joined_dataFrame['efeETG_GB'].mean()) / joined_dataFrame['efeETG_GB'].std()
+joined_dataFrame['Ati'] = (joined_dataFrame['Ati'] - joined_dataFrame['Ati'].mean()) / joined_dataFrame['Ati'].std()
+joined_dataFrame['Ate'] = (joined_dataFrame['Ate'] - joined_dataFrame['Ate'].mean()) / joined_dataFrame['Ate'].std()
+joined_dataFrame['An'] = (joined_dataFrame['An'] - joined_dataFrame['An'].mean()) / joined_dataFrame['An'].std()
+joined_dataFrame['q'] = (joined_dataFrame['q'] - joined_dataFrame['q'].mean()) / joined_dataFrame['q'].std()
+joined_dataFrame['smag'] = (joined_dataFrame['smag'] - joined_dataFrame['smag'].mean()) / joined_dataFrame['smag'].std()
+joined_dataFrame['x'] = (joined_dataFrame['x'] - joined_dataFrame['x'].mean()) / joined_dataFrame['x'].std()
+joined_dataFrame['Ti_Te'] = (joined_dataFrame['Ti_Te'] - joined_dataFrame['Ti_Te'].mean()) / joined_dataFrame['Ti_Te'].std()
+
+# Shuffles dataset
+shuffled_joined_dataFrame = joined_dataFrame.reindex(numpy.random.permutation(
+                                                joined_dataFrame.index))
+
+# Creates a pandas dataframe for the outputs
+shuffled_clean_output_df = shuffled_joined_dataFrame['efeETG_GB']
+
+# Creates a pandas dataframe for the inputs
+shuffled_clean_input_df = shuffled_joined_dataFrame.drop('efeETG_GB', axis=1)
+
+# Creates training dataset (90% of total data) for outputs
+y_train = shuffled_clean_output_df.iloc[:int(
+    numpy.round(len(shuffled_clean_output_df)*0.9))]
+
+# Creates training dataset (90% of total data) for inputs
+x_train = shuffled_clean_input_df.iloc[:int(
+    numpy.round(len(shuffled_clean_input_df)*0.9))]
+
+# Creates testing dataset (10% of total data) for outputs
+y_test = shuffled_clean_output_df.iloc[int(
+    numpy.round(len(shuffled_clean_output_df)*0.9)):]
+
+# Creates testing dataset (10% of total data) for inputs
+x_test = shuffled_clean_input_df.iloc[int(
+    numpy.round(len(shuffled_clean_input_df)*0.9)):]
+
+# Deletes pandas dataframes that are no longer needed
+del target_df, input_df
+
+# Closes the HDFStore. This is good practice.
+store.close()
+
+'''
+# Layers (FYI)
+Input_Layer = 7
+Hidden_Layer_1 = 30
+Hidden_Layer_2 = 30
+Output_Layer = 1
+'''
+
+# Define neural network
+model = Sequential()
+model.add(Dense(30,
+        input_shape=(7,),
+        activation='tanh',
+        kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None),
+        kernel_regularizer=regularizers.l2(0.00000001)))
+model.add(Dense(30,
+        activation='tanh',
+        kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None),
+        kernel_regularizer=regularizers.l2(0.00000001)))
+model.add(Dense(1,
+        activation='linear'))
+model.summary()
+
+
+model.compile(loss='mean_squared_error',   #categorical_crossentropy
+              optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999),
+              metrics=['accuracy', "mae", "mean_squared_error", rmse])
+
+# Add CallBacks (including TensorBoard)
+tbCallBack = keras.callbacks.TensorBoard(
+        log_dir='TensorBoard_logs/' + str(file_name), histogram_freq=5)
+EarlyStoppingCallBack = keras.callbacks.EarlyStopping(
+        monitor='val_rmse', min_delta=0, patience=15, verbose=0, mode='auto')
+
+history = model.fit(x = x_train,
+                    y = y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    shuffle=True,
+                    verbose=2,
+                    validation_data=(x_test, y_test),
+                    callbacks=[tbCallBack, EarlyStoppingCallBack])
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+print("score")
+print(score)
+
+print("model.metrics_names")
+print(model.metrics_names)
+
+# Create output file
+OutputFile = open("./Loss-Values/" +str(file_name) +".txt", "w+")
+OutputFile.write("Test loss: " + str(score[0]) + "\n")
+OutputFile.write("Test accuracy: " + str(score[1]) + "\n")
+OutputFile.write("val_mean_absolute_error: " +str(score[2]) + "\n")
+OutputFile.write("val_mean_squared_error: " +str(score[3]) + "\n")
+OutputFile.write("RMSE: " +str(score[4]) + "\n")
+OutputFile.close()
+
+# creates a HDF5 file 'my_model.h5'
+model.save("./Saved-Networks/" + str(file_name) +".h5")
