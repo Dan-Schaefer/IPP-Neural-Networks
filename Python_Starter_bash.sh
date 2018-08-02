1f3aa3a315970ecc7c5918951a2c9e77e18ef62c
Ephecina <danielschaefer01@gmail.com>
2018-08-02 12:56:00 +0200

    Update
    Lots of new files.

---
+++ Python_Starter_bash.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+sleep 3
+pwd
+#cd C:/Users/danie/OneDrive/Desktop/Python_Test/
+#cd pwd
+python 2018-07-24_Run0061.py
+sleep 3
+python 2018-07-24_Run0062.py
+sleep 3
+python 2018-07-24_Run0063.py
+sleep 3
+python 2018-07-24_Run0064.py
+sleep 3
+python 2018-07-24_Run0065.py
+sleep 3
+python 2018-07-24_Run0066.py
+sleep 3
+$SHELL
